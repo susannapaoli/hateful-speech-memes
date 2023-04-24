1772fbc7cc710a97fdac8f6fa3261fe3d11682d7
PierluigiManc01 <106546252+PierluigiManc01@users.noreply.github.com>
2023-04-24 19:18:28 -0400

    change in prediction from [1] to [0]

--- Utils/multimodal_trainer.py
+++ Utils/multimodal_trainer.py
@@ -157,7 +157,7 @@ def test_classify(image_model, text_model, fusion_model, test_loader, criterion,
           Prediction
           '''
           print("output:",output)
-          predicted = torch.max(output, dim=1)[1]
+          predicted = torch.max(output, dim=1)[0]
           print("prediction:",predicted)
           print("target:",target)
           correct += (predicted.float() == target.float()).sum().item()
